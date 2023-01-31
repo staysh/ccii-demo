b5ffdf516788a1e2b8ddafeb80db9ad8058098b0
staysh <nathan.gammill@pcc.edu>
2023-01-31 11:58:38 -0800

    changed bg

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 function setup () {
 	createCanvas(400, 400);
 	background(0);
-	fill(255);
+	fill(255, 200, 200);
 	ellipse(width/2, height/2, 100, 100);
+}
+
+function draw () {
+	ellipse(random(width), random(height), 100, 100);
 }
