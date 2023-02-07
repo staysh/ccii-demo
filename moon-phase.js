634b66c317511115f1625ea945dee9148f576362
staysh <nathan.gammill@pcc.edu>
2023-02-07 11:32:21 -0800

    changed args to object

--- moon-phase.js
+++ moon-phase.js
@@ -1,19 +1,35 @@
 let phaseArg = 0;
 
+let parameters = {
+		x: 0,
+		y: 0,
+		radius: 0,
+		phase: 0
+};
+
 function setup () {
-	createCanvas(700, 700);
+	var cnv = createCanvas(windowWidth, windowHeight);
+	cnv.style('display', 'block');
 	//noStroke();
+	parameters.x = width / 2;
+	parameters.y = height / 2;
+	parameters.radius = 100;
 }
 
 function draw () {
 	background(210, 180, 220);
 	
-	phaseArg += 0.01;
-	if (phaseArg > 1.0) {
-		phaseArg = 0;
+	parameters.phase += 0.01;
+	if (parameters.phase > 1.0) {
+		parameters.phase = 0;
 	}
 
 
+	fill(0);
+	ellipse(width / 2, height / 2, 200, 200);
+	drawMoon(parameters);
+
+	/*
 	for(let i = 0; i < 9; i++){
 		let phase = i / 8;
 		phase += phaseArg;
@@ -28,33 +44,39 @@ function draw () {
 		ellipse(x, y, r*2, r*2);
 		drawMoon(x, y, r, phase);
 	}
+	*/
 }
 
-function drawMoon(x, y, radius, phase) {
+function drawMoon(params) {
 	//let radius = r;
 	let lext;
 	let rext; 
-	if(phase <= 0.5) {
-		rext = radius * 4/3;
-		lext = map(phase, 0, 0.5, rext, -rext);
+	if(params.phase <= 0.5) {
+		rext = params.radius * 4/3;
+		lext = map(params.phase, 0, 0.5, rext, -rext);
 		
 	} else {
-		lext = -radius * 4/3;
-		rext = map(phase, 0.5, 1.0, -lext, lext);
+		lext = -params.radius * 4/3;
+		rext = map(params.phase, 0.5, 1.0, -lext, lext);
 	}
 	push();
-	translate(x, y);
+	translate(params.x, params.y);
 	fill(255);
 	beginShape();
-		vertex(0, radius);
-		bezierVertex(rext, radius, rext, -radius, 0, -radius);
-		bezierVertex(lext, -radius, lext, radius, 0, radius);
+		vertex(0, params.radius);
+		bezierVertex(rext, params.radius, rext, -params.radius, 0, -params.radius);
+		bezierVertex(lext, -params.radius, lext, params.radius, 0, params.radius);
 	endShape();
 	//noFill();
 	//ellipse(0, 0, width/2, width/2);
 	pop();
 }
 
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	parameters.x = width / 2;
+	parameters.y = height / 2;
+}
 // top = (0, radius)
 // bottom = (0, -radius)
 // 133 / 200 = 2/3 = 0.66666666
